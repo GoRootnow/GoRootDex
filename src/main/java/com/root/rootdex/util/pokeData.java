f80d81bfc31aa2b1db0014d7926a381c3dc2f112
GoRootNow <mrrootcreations@gmail.com>
2023-01-16 15:42:11 -0300

    config gradle

--- src/main/java/com/root/rootdex/util/pokeData.java
+++ src/main/java/com/root/rootdex/util/pokeData.java
@@ -12,9 +12,7 @@ import java.time.Instant;
 
 public class pokeData {
 
-
-
-    PokemonDto pikachu = new PokemonDto(25,"Pikachu", ELECTRIC,null);
+    public static PokemonDto pikachu = new PokemonDto(25,"Pikachu", ELECTRIC,null);
     PokemonDto bulbasaur = new PokemonDto(01,"Bulbasaur", GRASS,null);
     PokemonDto charmander = new PokemonDto(04,"Charmander", FIRE,null);
     PokemonDto squirtle = new PokemonDto(07,"Squirtle", WATER,null);
