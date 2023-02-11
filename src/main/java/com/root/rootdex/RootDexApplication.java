4a9411fffb30e621ab86f0cf7d7c1f786457a751
GoRootNow <mrrootcreations@gmail.com>
2023-02-11 16:33:20 -0300

    change app properties server port

--- src/main/java/com/root/rootdex/RootDexApplication.java
+++ src/main/java/com/root/rootdex/RootDexApplication.java
@@ -1,19 +1,24 @@
 package com.root.rootdex;
 
 import com.root.rootdex.Service.BinaryTree.Node;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RootDexApplication {
 
     public static void main(String[] args) {
-        //SpringApplication.run(RootDexApplication.class, args);
+        SpringApplication.run(RootDexApplication.class, args);
 
         // to do: Put this code on BinaryTree class
-
+        /*
         Node node = new Node();
         List<Integer> valuelist=new ArrayList<Integer>();
 
@@ -36,10 +41,8 @@ public class RootDexApplication {
         node = node.invert(node);
         System.out.println( "Inverted Tree");
         node.printBinaryTree(node);
-
-
-
-
+        */
     }
+
 
 }
